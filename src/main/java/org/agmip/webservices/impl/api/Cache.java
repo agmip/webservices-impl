522d7054c7338db8348c3ad10418137a543938c9
Christopher Villalobos <christopher.villalobos@gmail.com>
2012-05-23 13:42:32 -0400

    Starting map caching idea

---
+++ src/main/java/org/agmip/webservices/impl/api/Cache.java
@@ -0,0 +1,3 @@
+package org.agmip.webservices.impl.api;
+
+public interface Cache {}
--- src/main/java/org/agmip/webservices/impl/services/config/MetadataConfig.java
+++ src/main/java/org/agmip/webservices/impl/services/config/MetadataConfig.java
@@ -5,7 +5,7 @@ import org.codehaus.jackson.annotate.JsonProperty;
 public class MetadataConfig {
     @JsonProperty
     private RiakConfig riak = new RiakConfig();
-
+    
     public RiakConfig getRiakConfig() {
         return riak;
     }
