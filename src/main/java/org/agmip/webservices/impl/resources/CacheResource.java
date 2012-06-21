7d933b64a38a86bcf803ec9a32c2fb3058beee36
Christopher Villalobos <christopher.villalobos@gmail.com>
2012-06-21 11:07:25 -0400

    Starting cache resource

---
+++ src/main/java/org/agmip/webservices/impl/resources/CacheResource.java
@@ -0,0 +1,18 @@
+package org.agmip.webservices.impl.resources;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.MediaType;
+
+@Path("/cache/")
+@Produces(MediaType.APPLICATION_JSON)
+public class CacheResource {
+    
+    @Path("/map")
+    @GET
+    public String getMapCache() {
+    }
+}
